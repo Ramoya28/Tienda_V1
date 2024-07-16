7d8609455ac492f6eb76c46373ad4dee4f14670c
Ramoya28 <randallmontenegro87@gmail.com>
2024-07-15 21:07:32 -0600

    Commit semana 9
    Relación entre productos y categoria

--- src/main/java/com/tienda/domain/Producto.java
+++ src/main/java/com/tienda/domain/Producto.java
@@ -2,39 +2,39 @@ package com.tienda.domain;
 
 import jakarta.persistence.*;
 import java.io.Serializable;
-import java.util.List;
-import lombok.Data;                               
+import lombok.Data;
 
 @Data //especificar que es una capa de datos
 @Entity //Esta clase esta enlazada a una tabla en la bd
-@Table(name = "categoria") //Especificar cual entidad
-public class Categoria implements Serializable {
+@Table(name = "producto") //Especificar cual entidad
+public class Producto implements Serializable {
 
     private static final long seriaVersionUID = 1L; // auto incremento en Java MySQL (AUTO_INCREMENT)  
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_categoria")
+    @Column(name = "id_producto")
 
-    private Long idCategoria;
+    private Long idProducto;
+    private String detalle;
+    private int existencias;
+    private double precio;
     private String descripcion;
     private String rutaImagen;
     private boolean activo;
-    
-    @OneToMany
-    @JoinColumn(name = "id_categoria", updatable = false)
-    List<Producto> productos;
 
-    public Categoria() {
+    //private long idCategoria;
+    
+    @ManyToOne
+    @JoinColumn(name = "id_categoria")
+    Categoria categoria;
+
+    public Producto() {
     }
 
-    public Categoria(String categoria, boolean activo) {
-        this.descripcion = categoria;
+    public Producto(String producto, boolean activo) {
+        this.descripcion = producto;
         this.activo = activo;
     }
 
 }
-
-
- 
-
